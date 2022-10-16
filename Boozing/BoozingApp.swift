a85b0dc9ba9c0400f86b05e5226bb974c56e669e
Tobias <55360690+tbias25@users.noreply.github.com>
2022-10-16 15:59:41 +0200

    Initial Commit

---
+++ Boozing/BoozingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoozingApp.swift
+//  Boozing
+//
+//  Created by Tobias Abbenhaus on 16.10.22.
+//
+
+import SwiftUI
+
+@main
+struct BoozingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
